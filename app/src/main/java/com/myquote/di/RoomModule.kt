48f09b9766c4f53323905a731622bba1d22fc465
Gustavo Maldonado <gamcba7982@gmail.com>
2022-12-12 12:45:00 -0300

    fixed room module

--- app/src/main/java/com/myquote/di/RoomModule.kt
+++ app/src/main/java/com/myquote/di/RoomModule.kt
@@ -19,7 +19,7 @@ object RoomModule {
     @Singleton
     @Provides
     fun provideRoom(@ApplicationContext context: Context) =
-        Room.databaseBuilder(context, QuoteDatabase::class.java, QUOTE_DATABASE_NAME)
+        Room.databaseBuilder(context, QuoteDatabase::class.java, QUOTE_DATABASE_NAME).build()
 
     @Singleton
     @Provides
